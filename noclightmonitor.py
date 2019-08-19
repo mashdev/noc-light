a3396a83fd09ce884ad914f6e003e376ec81a7c8
Paul Lopez <paul.lopez@edgenuity.com>
2019-08-19 11:30:57 -0700

    update how url time is captured & added test slack

--- noclightmonitor.py
+++ noclightmonitor.py
@@ -4,50 +4,73 @@ import urllib3
 import sys
 import requests
 
-LongestApplicationTime = 0.0
+LongestApplicationTime = 0.150000
 LongestApplicationURL = ""
 ApplicationYellowThreshold = 2
 ApplicationRedThreshold = 4
-ApplicationTimeout = 6
+# ApplicationTimeout = 6
+ApplicationTimeout = 0.26000
 
-# #General Options
+#################
+# General Options
 # Green = 8
 # Yellow = 9
 # Red = 10
 # Strobe = 7
 
-# # Reverse logic here because a ground for the relay activates it
+##################################################################
+# Reverse logic here because a ground for the relay activates it
 # On = False
 # Off = True
 
 # def Light(pin, status):
 #   GPIO.output(pin,status)
 
-
+################################
+# print to Supervisor.d console
 # def console(message):
 #   print("%s : %s" % (datetime.datetime.now(), message))
 
 # apptimeout = 2
 # running = True
 
-# Array = ["test1", "test2", "test3"]
 # SummaryOfPoll = []
+fastestUrls = []
+
+red = "#FE0C2F"
+yellow = "#F9FE01"
+green = "#42F109"
 
 def checkApplicationLatency(a):
-  LongestApplicationTime = 0 
   try:
-    # start = time.time()  
-    timeToCall = requests.get(a, timeout=ApplicationTimeout).elapsed.total_seconds()
-    
-    # if timeToCall < 0.009:
-    #   print(a, " is the fastest url", timeToCall)
-    
+    # timeToCall = requests.get(a, timeout=LongestApplicationTime).elapsed.total_seconds()
+    start = time.time()
+    requests.get(a, stream=True).close()
+    end = time.time()
+    timeToCall = end - start
+    # print(timeToCall)
+
+    if timeToCall < 0.009:
+      fastestUrls.append(a)
+      print(a, "is the fastest")
+
     # if timeToCall > 0.110000:
     #   print(a, " is the slowest url", timeToCall)
-    print(timeToCall, " :", a)
-    # end = time.time()
-    # timeToCall = end - start
-    # print(timeToCall)
+    #   requests.post(
+    #     webhook_url,
+    #     json=slackMessage(text=a, color=red),
+    #     headers={'Content-Type': 'application/json'}
+    #   )
+    # elif 0.10000 < timeToCall:
+    #   print(a, " Fast")
+    #   requests.post(
+    #     webhook_url,
+    #     json=slackMessage(text=a, color=green),
+    #     headers={'Content-Type': 'application/json'}
+    #   )
+    # else:
+    #   pass
+
     #debug(i + " took " + str(timeToCall) + " seconds")
     
     # if timeToCall > LongestApplicationTime:
@@ -55,13 +78,21 @@ def checkApplicationLatency(a):
     #   LongestApplicationTime = timeToCall
     #   LongestApplicationURL = a
   except:
-    print(a," failed to load request")
+    print("slowest url is:", a)
+    requests.post(
+      webhook_url,
+      json=slackMessage(text=a, color=red),
+      headers={'Content-Type': 'application/json'}
+    )
+
+    # print(a,"timed out")
     # console(a)
     # console(sys.exc_info()[1])
     # SummaryOfPoll.append(a)
     # SummaryOfPoll.append(str('%.2f' % timeToCall))
     # return False
     
+  
   return True
 
 
@@ -70,6 +101,32 @@ def MonitorUrls(urls):
       # print(url)
       checkApplicationLatency(url)
     # time.sleep(1)
-    # return
+    # return fastestUrls
 
-# MonitorUrls(allUrls)
+webhook_url = "https://hooks.slack.com/services/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"
+
+def slackMessage(
+        fall="",
+        color="#7F7F80",
+        pre="",
+        title="Light Trigger Test",
+        text="test test test",
+        footer="NOC-LIGHT"):
+    showError = {
+        "attachments": [
+            {
+                "fallback": fall,
+                "color": color,
+                "pretext": pre,
+                "title": title,
+                "text": text,
+                "footer": footer,
+                "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                "ts": time.time()
+            }
+        ]
+    }
+    return showError
+
+
+
