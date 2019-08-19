909aa9324bcb0ee0765f0cb287d536ecab301f01
Paul Lopez <paul.lopez@edgenuity.com>
2019-08-19 11:28:23 -0700

    added commenting and removed hardcoded links

--- noclightcron.py
+++ noclightcron.py
@@ -22,25 +22,27 @@ import traceback
 # NOTE: use python -m pip install mysql-connector
 # to install mysql connector
 
+#########################
 #NOC Light Color Options
 Green = 8
 Yellow = 9
 Red = 10
 Strobe = 7
-
 DEBUG = False
 
+#####################
+# Loop length seconds
 LoopSleep = 7
+
 # Reverse logic here because a ground for the relay activates it
 On = False
 Off = True
-
 
 # def Light(pin, status):
 #   	GPIO.output(pin,status)
 
 
-######
+#####################
 # Database Connection
 sqldb = mysql.connector.connect(
   host="localhost",
@@ -49,47 +51,32 @@ sqldb = mysql.connector.connect(
   database="noclightaz"
 )
 
+#####################
+# create sql select statement
 geturls = sqldb.cursor(named_tuple=True)
 geturls.execute("SELECT urlEndpoint FROM endpoints")
 results = geturls.fetchall()
 scanUrl = []
 
+##########################
+# put db results to array
 for result in results:
   scanUrl.append(result.urlEndpoint)
 
+#########################
 #Internet monitor options
 TestURLList = ["http://www.google.com","http://www.yahoo.com","http://www.twitter.com"]
 TestURLTimeout = 5
-ApplicationCheckURLs = ["http://learn.education2020.com/educator/monitor.aspx",
-"https://learn.education2020.com/educator/monitor.aspx",
-"https://learn.education2020.com/contentviewers/monitor",
-"http://auth.edgenuity.com/login/login/student",
-"http://auth.edgenuity.com/login/login/educator",
-"https://edgenuity.slack.com",
-"http://core.learn.edgenuity.com/platformsynthesizer/EdgeStatus/FullStatus",
-"http://cp.edgenuity.com/Contentplatform/api/EdgeStatus/FullStatus", 
-"http://auth.edgenuity.com/Login/EdgeStatus/FullStatus",
-"http://auth.edgenuity.com/AuthenticationAPI/req/EdgeStatus/FullStatus",
-"http://tools.core.learn.edgenuity.com/businessapi/api/EdgeStatus/FullStatus"]
 
-ApplicationTimeout = 6
+
+# ApplicationTimeout = 6
 ## Setting below for testing purposes
 #ApplicationTimeout = .2
 
-UrlBase = "http://r{x}.core.learn.edgenuity.com/"
-ApplicationCheckBase = ["ContentViewers/EdgeStatus/FullStatus", "QuestionAPI/api/EdgeStatus/FullStatus", "ContentEngineAPI/api/EdgeStatus/FullStatus", "student/EdgeStatus/FullStatus", "Educator/EdgeStatus/FullStatus", "Family/EdgeStatus/FullStatus", "LMSAPI/req/EdgeStatus/FullStatus", "Player/EdgeStatus/FullStatus"]
-MinRealm = 2
-MaxRealm = 22
-realmCounter = MinRealm
 apptimeout = 5
 check = True
 
-while (realmCounter <= MaxRealm):
-  for s in ApplicationCheckBase:
-    ApplicationCheckURLs.append(("%s%s" % (UrlBase, s)).replace('{x}', "{:0>2d}".format(realmCounter)))
-  realmCounter += 1
 # debug(ApplicationCheckURLs)
-
 
 # postMsg = "NOC Light restarted, URLs built, polling started"
 # console(postMsg)
