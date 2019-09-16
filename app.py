1ba06bdc3e74a2cae537811180ad3d1a0f0f8937
Paul Lopez <paul.lopez@edgenuity.com>
2019-09-16 13:12:19 -0700

    add create route and fix check route

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, jsonify, json
+from flask import Flask, render_template, request, jsonify, json, redirect
 from flask_cors import CORS
 import mysql.connector, os
 import connection
 import time
+import requests
+from requests.exceptions import ConnectionError
 from dotenv import load_dotenv
 
 import urllib.request
@@ -14,11 +16,11 @@ load_dotenv()
 # instantiate app
 app = Flask(__name__)
 app.config.from_object(__name__)
-
 CORS(app, resources={r'/*': {'origins': '*'}})
-
 db = connection.DBConnection()
 
+###############
+# API Endpoints
 @app.route('/ping')
 def pong():
   return jsonify('pong')
@@ -27,12 +29,23 @@ def pong():
 def index():
   return 'Flask app running'
 
-
+# return list of current api's
 @app.route('/dashboard')
 def dashboard():
   results = db.getUrlList('SELECT * FROM endpoints WHERE urlEnabled = 1')
   return jsonify(results)
 
+@app.route('/create', methods=['GET','POST'])
+def create():
+  if request.method == 'POST':
+    for item in request.json['data']['params']:
+      print(item['url'])
+    return 'you request the post method'
+    
+  if request.method == 'GET':
+    return 'get method'
+  
+  # return redirect('/add/', 302)
 
 @app.route('/update/<id>', methods=['GET', 'POST'])
 def update(id):
@@ -47,57 +60,89 @@ def update(id):
 @app.route('/check/', methods=['GET', 'POST']) 
 def check():
   url = request.args.get('param')
+  try:
+    x = requests.get(url)
+    print("PRINTING STATUS CODE:")
+    print(x.status_code)
 
-  start = time.time()
-  x = urllib.request.urlopen(url)
-  end = time.time()
-  calltime = end-start
-  
-  if x.status == 200:
-    envelope = {
+    if x.status_code == 200:
+      start = time.time()
+      y = requests.get(url)
+      end = time.time()
+      calltime = end - start
+      envelope = {
+        "url" : url,
+        "statuscode" : y.status_code,
+        "calltime" : calltime,
+        "text" : {}
+      }
+      return envelope
+    elif x.status_code > 215:
+      print("HTTP ERROR CODE:"+ str(x.status_code))
+      err215 = {
+        "url" : url,
+        "statuscode" : x.status_code,
+        "text" : "Please check the url"
+      }
+      return err215
+    
+  ## Not needed but keeping these 
+  ## error exceptions for now
+  except(urllib.error.URLError):
+    print( "FAILED RESULTS ARE: ")
+    error1 = {
       "url" : url,
-      "body" : x.status,
-      "calltime" : '{0:.2g}'.format(calltime)
+      "statuscode" : 404,
+      "text" : "THE WEBSITE DOES NOT EXIST"
     }
-    return envelope
-  
-  else:
-    failed = {
+    return error1
+
+  except ConnectionError:
+    print("UNABLE TO CONNECT TO SITE")
+    error2 = {
       "url" : url,
-      "body" : x.status,
-      "calltime" : '{0:.2g}'.format(calltime)
+      "statuscode" : 404,
+      "text" : "UNABLE TO CONNECT TO SITE"
     }
+    return error2
 
-  # try:
-  #   if x.status == 200:
-  #     print(x.read())
-  #     return x.read()
+  except requests.RequestException:
+    print("Ambigous request, try again")
+    error3 = {
+      "url" : url,
+      "statuscode" : 404,
+      "text" : "Ambigous request, try again"
+    }
+    return error3
+
+  except requests.URLRequired:
+    print("A VALID URL IS REQUIRED")
+    error4 = {
+      "url" : url,
+      "statuscode" : 404,
+      "text" : "A VALID URL IS REQUIRED"
+    }
+    return error3
+
+  except requests.Timeout:
+    print("CONNECTION TO ENDPOINT OR URL TIMED OUT")
+    error5 = {
+      "url" : url,
+      "statuscode" : 404,
+      "text" : "CONNECTION TO ENDPOINT OR URL TIMED OUT"
+    }
+    return error5
   
-  # except:
-  #   if x.status == 500 | x.status == 400 | x.status == 404:
-  #     print("API FAILED TO RESOLVE TO URL")
-  #     return
-
-  # try:
-  #   start = time.time()
-  #   x = urllib.request.urlopen(url)
-  #   end = time.time()
-  #   total = end   - start
-  #   print(end - start)
-
-  #   envelope = {
-  #     "url" : url,
-  #     "body" : x.status,
-  #     "calltime" : '{0:.2g}'.format(total)
-  #   }
-  #   return envelope
-
-  # except:
-  #   x = urllib.request.urlopen(url)
-  #   if x.status == 500:
-  #     print("API Url is not reachable")
-  #   return
+  except:
+    print("throw exception")
+    error6 = {
+      "url" : url,
+      "statuscode" : 404,
+      "text" : "UNEXPECTED EXCEPTION, SORRY CHARLIE"
+    }
+    return error6
 
 
 if __name__ == '__main__':
   app.run()
+
--- client/src/views/AddUrl.vue
+++ client/src/views/AddUrl.vue
@@ -1,86 +1,92 @@
 <template>
   <div>
     <div class="container">
-      <p class="left">Add Url</p>
+      <p class="left slim">Enter URL Endpoint(s)</p>
       <textarea 
         name="urltext" 
         id="urlstext" 
         class="url-text-area" 
         v-model="urltext" 
-        placeholder="Enter comma separated list"
-        cols="10" 
+        v-on:keyup="ckSubmitBtn"
+        v-on:mouseup="ckSubmitBtn"
+        placeholder="(For multiple endpoints separate with a comma)"
         rows="10"
       ></textarea>
       <div class="btn-area left">
-        <a class="waves-effect waves-teal btn-flat" v-on:click="separateAndTest()"> Test </a>
+        <a class="waves-effect waves-light btn-flat check-url-btn" v-show="submitBtn" v-on:click="separateAndTest(), showInsertButton()"> Check URL
+        </a>
+
+        <a class="waves-effect waves-red btn-flat" v-show="resetBtn" v-on:click="clearResults()">    clear
+        </a>
       </div>
-      <div class="formatted-urls">
-      
-        <table>
-          <tbody
-            v-for="(get, index) in reformatted" 
-            v-bind:key="index"
-            class=""
-          >
-            <tr>
-              <td>
-              {{ index+1 }}
+
+    <!-- <form action=""> -->
+      <table class="url-tbl highlight">
+        <tbody
+          v-for="(get, index) in reformatted"
+          v-bind:key="index"
+        >
+          <div class="tbl-row">
+            <tr v-if="get.statuscode < 215">
+              <td class="green-light">
               </td>
               <td>
                 URL: {{ get.url }}
               </td>
               <td>
-                Response Time: {{ get.calltime }}
+                Call Time: {{ get.calltime.toFixed(2) }}
+              </td>
+              <td class="green-text">
+                Status Code: {{ get.statuscode }}
               </td>
               <td>
-                Status Code: {{ get.body }} -- Success
+                <SelectMenu></SelectMenu>
               </td>
             </tr>
-          </tbody>
-        </table>
+          </div>
+        </tbody>
+          <tr v-if="insertBtn">
+            <td class="insert-btn-row-td center" colspan="4"> 
+              <button class="btn insert-btn" v-on:click="submitEndPointUrls()">
+                Insert
+              </button> 
+            </td>
+          </tr>
 
-        <!-- <table>
-          <tbody
-            v-for="(get, index) in failed" 
-            v-bind:key="index"
-            class=""
-          >
-            <tr>
-              <td>
-              {{ index+1 }}
-              </td>
-              <td>
-                URL: {{ get.url }}
-              </td>
-              <td>
-                Response Time: {{ get.calltime }}
-              </td>
-              <td>
-                Status Code: {{ get.body }} -- Success
-              </td>
-            </tr>
-          </tbody>
-        </table> -->
-
-{{ failed }}
-
-
-        <p> {{ urlsTotal }} </p>
-        
-      </div>
-    </div>
-
-
-
+        <tbody
+          v-for="(fail, index) in failures"
+          v-bind:key="index"
+        >
+        <div class="tbl-row">
+          <tr v-if="fail.statuscode > 215">
+            <td class="red-light">
+            </td>
+            <td>
+              URL: {{ fail.url }}
+            </td>
+            <td>
+              Call Time: N/A
+            </td>
+            <td class="red-text">
+              Status Code: <span> {{ fail.statuscode }} </span>
+            </td>
+          </tr>
+        </div>
+        </tbody>
+      </table>
+    <!-- </form> -->
+  </div>
   </div>
 </template>
 
 <script>
 import Icons from '@/components/Icons'
+import SelectMenu from '@/components/SelectMenu'
 
 export default {
   components: {
-    Icons
+    Icons,
+    SelectMenu
   },
   data() {
     return {
@@ -88,9 +94,13 @@ export default {
       separated: [],
       urlsTotal: null,
       reformatted: [],
-      failed: [],
-      icon: 'add',
-      url: 'http://localhost:5000/check/?param='
+      failures: [],
+      baseUrl: 'http://127.0.0.1:5000',
+      url: 'http://localhost:5000/check/?param=',
+      submitBtn : false,
+      resetBtn : false,
+      insertBtn : false,
+      selected: ''
     }
   },
 
@@ -98,69 +108,153 @@ export default {
     separateAndTest() {
       this.separated = this.urltext.split(',');
       this.urlsTotal = this.separated.length;
-      let res = this.reformatted
-      let failed = this.failed
-      
+      let res = this.reformatted;
+      let fails = this.failures;
+
+      fails.length = 0;
       res.length = 0;
 
       for(let i=0; i < this.urlsTotal; i++){
-        axios.get( this.url + this.separated[i] )
+        try {
+          axios.get( this.url + this.separated[i] )
           .then(function(response) {
-            res.push(response.data);
-            
+            // console.dir(response)
+
+            if(response.data.statuscode == 200){
+              console.log("API Call Successful")
+              res.push(response.data);
+            }
+            else if (response.data.statuscode > 300){
+              console.log("API CALL FAILED: "+ response.data.statuscode);
+              fails.push(response.data)
+            }
           })
           .catch(function(errs) {
-            failed.push(errs.data);
-        })
-      }
+            fails.push("response.data failed: "+ errs);
+          })
+        } catch (error) {
+          console.log("An error occurred: "+error);
+          fails.push(response.data)
+        }
+      } //end for loop
 
-      // for(let i=0; i < this.urlsTotal; i++){
-      //   setInterval(function(){
-      //     console.log("sleeping for half second")  
-      //   },1000)
-      // }
-            // this.reformatted.push(this.separated[i])
-          
-        
-        // return axios.get(url + this.separated[i])
-        // .then(function(res) {
-        //   // console.log(res.status)
-
-        //   if(res.status === 200) {
-        //     console.log("Success: ", res)
-        //     this.reformatted.push( this.separated[i] + res.status)
-        //   }
-        //   else {
-        //     console.log("Call to the API Failed, please adjust endpoint and try again.")
-        //     this.reformatted.push( "FAILED ",this.separated[i] )
-        //   }
-        // })
-        // .catch(function(err) {
-        //   console.log("SOMETHING JUST BROKE!!: ", err, this.separated[i]);
-        // })
-    
 
       
-        
     },
 
-    testUrl(id) {
- 
+    ckSubmitBtn() {
+      if(this.urltext.length > 10 && this.urltext != null) {
+        this.submitBtn = true;
+        this.resetBtn = true;
+      }
+      else {
+        this.submitBtn = false;
+        this.resetBtn = false;
+      }
     },
 
+    clearResults() {
+      this.reformatted.length = 0;
+      this.failures.length = 0;
+      this.urltext = '';
+      this.resetBtn = false;
+      this.submitBtn = false;
+    },
 
-  },
+    showInsertButton() {
+      setInterval(() => {
+        if(this.reformatted.length > 0) {
+          this.insertBtn = true;
+        }
+        else {
+          this.insertBtn = false;
+        }
+      }, 700)
+    },
 
-  computed: {
+    submitEndPointUrls() {
+        // console.log(urlArr);
+        axios.post(this.baseUrl+'/create', {
+          data: {
+            params : this.reformatted
+          }
+        })
+    }
 
-  }
-  
-}
+
+  } //end methods:
+
+} //end export default()
 </script>
 
 <style scoped>
-  .url-text-area{
-    height: 15%;
-    width: 100%;
-  }
+table {
+  width: 100%;
+}
+
+tr {
+  width: 100%;
+}
+
+.tbl-row {
+  padding-bottom: 10px;
+  width: 100%;
+}
+
+td {
+  padding: 5px;
+  margin: 5px 0px 5px 0px;
+  border-radius: 0;
+}
+
+.url-text-area{
+  height: 15%;
+  width: 100%;
+}
+
+.green-light {
+  background-color: green;
+  padding: 5px 3px 5px 3px;
+}
+
+.red-light {
+  background-color: red;
+  padding: 5px 3px 5px 3px;
+}
+
+.tbl-row-err {
+
+}
+
+.red-text {
+  color: red;
+}
+
+.green-text {
+  color: green;
+}
+
+.slim {
+  padding: 0px;
+  margin: 0px;
+}
+
+.insert-btn-row-td {
+  padding-bottom: 2%;
+}
+
+.btn-area {
+  margin-bottom: 2%;
+}
+
+.check-url-btn {
+  background-color: orange;
+}
+
+.insert-btn {
+  background-color: green;
+}
+
+
+
 </style>
