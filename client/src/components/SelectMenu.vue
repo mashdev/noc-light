c34e105be3278cd7c4327bb7039d23d1bec371d1
Paul Lopez <paul.lopez@edgenuity.com>
2019-09-16 13:15:09 -0700

    initial commit

---
+++ client/src/components/SelectMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="input-field">
+      <select>
+        <option value="" disabled selected>Choose your option</option>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+      </select>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  
+</style>
