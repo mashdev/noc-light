4ac9da3172b1311ddd39beda4b3c8a02ea48a70c
Paul Lopez <paul.lopez@edgenuity.com>
2019-10-03 11:09:45 -0700

    old .vue renamed to Home.vue

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/noc-seal.jpg">
+    <HomeDash class="home-dash" msg="NOC Light Dashboard"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HomeDash from '@/components/HomeDash.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    HomeDash,
   },
 };
 </script>
+
+
+<style scoped>
+
+img {
+  height: 180px;
+  width: 180px;
+  margin: 25px 0px 0px 0px;
+}
+
+.home-dash {
+  margin-top: 25px;
+}
+
+</style>
