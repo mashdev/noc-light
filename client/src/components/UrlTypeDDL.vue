18de18ee765153d182c07b6e1bcbb755e9fa7d96
Paul Lopez <paul.lopez@edgenuity.com>
2019-10-03 11:10:59 -0700

    initial commit

---
+++ client/src/components/UrlTypeDDL.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="input-field" v-for="items in selectMenu">
+      <select>
+        <option value="1"> {{ items.value }} </option>
+      </select>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'urltype',
+  ],
+
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style scoped>
+  /*   */
+
+</style>
