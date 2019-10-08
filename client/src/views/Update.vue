89f84c8601a5c313ba5b69072e0f5c454d6082bf
Paul Lopez <paul.lopez@edgenuity.com>
2019-10-08 16:28:46 -0700

    added function to manipulate record and submit

--- client/src/views/Update.vue
+++ client/src/views/Update.vue
@@ -5,23 +5,40 @@
       <h3>Edit Endpoint Update</h3>
       </div>
 
-      <div v-for="(result, id) in url" v-bind:key="id" class="row" >
+      <div 
+        v-for="(result, id) in urlsArr[0]" 
+        v-bind:key="id" 
+        class="row"
+      >
         <div class="col-md-6 text-left">
           <span class=""> Endpoint </span>
-          <input type="text" v-bind:value="result[1]" class="form-control">
+            <input 
+              type="text" 
+              class="form-control"
+              id="endpoint-url"
+              
+              v-model="urlEndpointCheck"
+            >
         </div>
-
         <div class="col-md-3 text-left">
           Url Type:
-          <select
-            name=""
-            id=""
-            class="form-control"
-            v-model="urlType"
-          >
-            <option value="0">0</option>
-            <option value="1">1</option>
-        </select>
+          <div>
+            <select
+              name=""
+              id=""
+              class="form-control"
+              v-model="urlType"
+            >
+              <option 
+                v-for="(result2, id2) in urlsArr[1]" 
+                v-bind:key="id2"
+                :value="result2.catid"
+              >
+                {{ result2.categoryName }}
+              </option>
+            </select>
+          </div>
+
         </div>
 
         <div class="col-md-3 text-left">
@@ -31,55 +48,138 @@
             id=""
             class="form-control"
             v-model="isEnabled"
+            v-on:change="selectIsEnabled($event)"
           >
-          <!-- use for loop to assign values -->
-            <option value="0" selected> Disabled </option>
-            <option value="1"> Enabled </option>
+          
+            <option 
+              v-for="(val, index) in enabledList"
+              v-bind:key="index"
+              :value="val.id"
+            >
+              {{ val.name }}
+            </option>
+            
           </select>
         </div>
 
+        <button 
+          class="btn btn-warning submit-btn"
+          @click="checkUrlAndSubmit(urlEndpointCheck, urlId)"
+        >
+          Submit
+        </button>
+      Endpoint: {{ urlEndpointCheck }} <br>
+      URL Type:{{ urlType }} <br>
+      Enabled: {{ isEnabled }} 
+      PostID: {{idForUpdate}}
       </div>
-
+      
 
   </div>
 </template>
 
 <script>
+
 export default {
   components: {},
 
   data() {
     return {
       urlId: this.$route.params.id,
-      url: '',
+      urlsArr: '',
+      enabledList: [
+        {name: 'Disabled', id: 0},
+        {name: 'Enabled', id: 1}
+      ],
       isEnabled: '',
       urlType: '',
+      urlEndpoint: '',
+      urlEndpointCheck: '',
+      urlCategories: [],
+      checkUrl: 'http://localhost:5000/check/?param=',
     };
   },
 
   methods: {
     updateById(id) {
-      return axios.get(`http://localhost:5000/update/${this.urlId}`)
-        .then((response) => {
-          console.log(response.data);
-          this.url = response.data;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      return axios.get(`http://localhost:5000/update/${id}`)
+      .then((response) => {
+        this.urlsArr = response.data
+        this.urlCategories = response.data.categories
+        this.urlType = response.data[0][0].catid
+        this.isEnabled = response.data[0][0].urlEnabled
+        this.urlEndpointCheck = response.data[0][0].urlEndpoint
+        this.idForUpdate = response.data[0][0].id
+      })
+      .catch((error) => {
+        console.log(error);
+      })
     },
+
+    selectIsEnabled(event) {
+      this.isEnabled = event.target.value
+      console.log(event.target.value);
+    },
+
+    checkUrlAndSubmit(url,urlId){
+      this.urlEndpointCheck
+
+      return axios.get(`http://localhost:5000/check/?param=${url}`)
+      .then( (response) => {
+        try {
+          if(response.data.statuscode == 200) {
+            axios.post(`http://localhost:5000/update/${urlId}`, {
+              data : {
+                params : {
+                  endpoint: this.urlEndpointCheck,
+                  urltype: this.urlType,
+                  isenabled: this.isEnabled,
+                  uid: this.urlId
+                },
+                headers : {
+                  'Content-Type': 'application/json'
+                }
+              }
+            })
+            .then((response) => {
+              console.log(response)
+              alert("Successfully Updated Endpoint")
+              this.$router.push('/dashboard')
+            })
+            .catch((error) => {
+              console.log(error)
+              alert(error)
+            })
+            
+            // console.log(this.urlEndpointCheck+" "+this.urlType+" "+this.isEnabled )
+            // axios.post(`http://localhost:5000/update/${url}`)
+            
+          }
+          else if(response.data.statuscode >= 400) {
+            console.log(response.data)
+            alert(response.data.text)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      })
+    }
+
   },
 
-  computed: {
-
+  mounted() {
+    this.updateById(this.urlId);
   },
 
-  created() {
-    this.updateById();
-  },
-};
+
+
+}
 </script>
 
 <style scoped>
+
+.submit-btn {
+  margin: 10px 15px;
+}
 
 </style>
