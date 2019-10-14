cd763941315f779dc5aca4c3104fc03add6fa72a
Paul Lopez <paul.lopez@edgenuity.com>
2019-10-14 13:51:57 -0700

    text and line formatting

--- client/src/views/Update.vue
+++ client/src/views/Update.vue
@@ -5,18 +5,18 @@
       <h3>Edit Endpoint Update</h3>
       </div>
 
-      <div 
-        v-for="(result, id) in urlsArr[0]" 
-        v-bind:key="id" 
+      <div
+        v-for="(result, id) in urlsArr[0]"
+        v-bind:key="id"
         class="row"
       >
         <div class="col-md-6 text-left">
           <span class=""> Endpoint </span>
-            <input 
-              type="text" 
+            <input
+              type="text"
               class="form-control"
               id="endpoint-url"
-              
+
               v-model="urlEndpointCheck"
             >
         </div>
@@ -29,8 +29,8 @@
               class="form-control"
               v-model="urlType"
             >
-              <option 
-                v-for="(result2, id2) in urlsArr[1]" 
+              <option
+                v-for="(result2, id2) in urlsArr[1]"
                 v-bind:key="id2"
                 :value="result2.catid"
               >
@@ -50,38 +50,32 @@
             v-model="isEnabled"
             v-on:change="selectIsEnabled($event)"
           >
-          
-            <option 
+
+            <option
               v-for="(val, index) in enabledList"
               v-bind:key="index"
               :value="val.id"
             >
               {{ val.name }}
             </option>
-            
+
           </select>
         </div>
 
-        <button 
+        <button
           class="btn btn-warning submit-btn"
           @click="checkUrlAndSubmit(urlEndpointCheck, urlId)"
         >
           Submit
         </button>
-        <button 
+        <button
           class="btn btn-secondary submit-btn"
           v-on:click="cancelUpdate"
         >
         Cancel
         </button>
-
-      Endpoint: {{ urlEndpointCheck }} <br>
-      URL Type:{{ urlType }} <br>
-      Enabled: {{ isEnabled }} 
-      PostID: {{idForUpdate}}
-
       </div>
-      
+
 
   </div>
 </template>
@@ -96,8 +90,8 @@ export default {
       urlId: this.$route.params.id,
       urlsArr: '',
       enabledList: [
-        {name: 'Disabled', id: 0},
-        {name: 'Enabled', id: 1}
+        { name: 'Disabled', id: 0 },
+        { name: 'Enabled', id: 1 },
       ],
       isEnabled: '',
       urlType: '',
@@ -111,78 +105,76 @@ export default {
   methods: {
     updateById(id) {
       return axios.get(`http://localhost:5000/update/${id}`)
-      .then((response) => {
-        this.urlsArr = response.data
-        this.urlCategories = response.data.categories
-        this.urlType = response.data[0][0].catid
-        this.isEnabled = response.data[0][0].urlEnabled
-        this.urlEndpointCheck = response.data[0][0].urlEndpoint
-        this.idForUpdate = response.data[0][0].id
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+        .then((response) => {
+          this.urlsArr = response.data;
+          this.urlCategories = response.data.categories;
+          this.urlType = response.data[0][0].catid;
+          this.isEnabled = response.data[0][0].urlEnabled;
+          this.urlEndpointCheck = response.data[0][0].urlEndpoint;
+          this.idForUpdate = response.data[0][0].id;
+        })
+        .catch((error) => {
+          console.log(error);
+          alert(error)
+        });
     },
 
     selectIsEnabled(event) {
-      this.isEnabled = event.target.value
+      this.isEnabled = event.target.value;
       console.log(event.target.value);
     },
 
-    checkUrlAndSubmit(url,urlId){
-      this.urlEndpointCheck
+    checkUrlAndSubmit(url, urlId) {
+      this.urlEndpointCheck;
 
       return axios.get(`http://localhost:5000/check/?param=${url}`)
-      .then( (response) => {
-        try {
-          if(response.data.statuscode == 200) {
-            axios.post(`http://localhost:5000/update/${urlId}`, {
-              data : {
-                params : {
-                  endpoint: this.urlEndpointCheck,
-                  urltype: this.urlType,
-                  isenabled: this.isEnabled,
-                  uid: this.urlId
+        .then((response) => {
+          try {
+            if (response.data.statuscode == 200) {
+              axios.post(`http://localhost:5000/update/${urlId}`, {
+                data: {
+                  params: {
+                    endpoint: this.urlEndpointCheck,
+                    urltype: this.urlType,
+                    isenabled: this.isEnabled,
+                    uid: this.urlId,
+                  },
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
                 },
-                headers : {
-                  'Content-Type': 'application/json'
-                }
-              }
-            })
-            .then((response) => {
-              console.log(response)
+              })
+                .then((response) => {
+                  console.log(response);
 
-              if(response.status == 204) {
-                alert('No changes detected');
-                this.$router.push('/dashboard');
-              }
-              else if(response.status == 200) {
-                alert("Endpoint successfully updated.");
-                this.$router.push('/dashboard');
-              }
-            })
-            .catch((error) => {
-              console.log(error)
-              alert(error)
-            })
-            
-            // console.log(this.urlEndpointCheck+" "+this.urlType+" "+this.isEnabled )
-            // axios.post(`http://localhost:5000/update/${url}`)
-            
+                  if (response.status == 204) {
+                    alert('No changes detected');
+                    this.$router.push('/dashboard');
+                  } else if (response.status == 200) {
+                    alert('Endpoint successfully updated.');
+                    this.$router.push('/dashboard');
+                  }
+                })
+                .catch((error) => {
+                  console.log(error);
+                  alert(error);
+                });
+
+              // console.log(this.urlEndpointCheck+" "+this.urlType+" "+this.isEnabled )
+              // axios.post(`http://localhost:5000/update/${url}`)
+            } else if (response.data.statuscode >= 400) {
+              console.log(response.data);
+              alert(response.data.text);
+            }
+          } catch (error) {
+            console.log(error);
           }
-          else if(response.data.statuscode >= 400) {
-            console.log(response.data)
-            alert(response.data.text)
-          }
-        } catch (error) {
-          console.log(error)
-        }
-      })
+        });
     },
 
     cancelUpdate() {
-      this.$router.push('/dashboard')
-    }
+      this.$router.push('/dashboard');
+    },
 
   },
 
@@ -191,8 +183,7 @@ export default {
   },
 
 
-
-}
+};
 </script>
 
 <style scoped>
