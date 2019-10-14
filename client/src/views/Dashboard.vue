e7aec4bb728ad2fb0864354f0a51cf8686299650
Paul Lopez <paul.lopez@edgenuity.com>
2019-10-14 13:51:35 -0700

    add function to sort by column  axios methods

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="container">
     <h3>NOC Light Urls</h3>
+    <i class="material-icons"> {{sort ? arrowup : arrowdown }} </i>
         <table class="table table-sm">
           <thead>
             <tr class="text-left">
-              <th scope="col">API Endpoint</th>
-              <th scope="col">URL Type</th>
-              <th scope="col">Enabled</th>
-              <th scope="col">Update</th>
+              <th scope="col" > 
+                <span 
+                  @click="sortByColumn('urlEndpoint')"
+                  class="sort-by"
+                > API Endpoint 
+                </span>
+              </th>
+              <th scope="col">
+                <span 
+                  @click="sortByColumn('urlType')"
+                  class="sort-by"
+                > URL Type 
+                </span>
+              </th>
+              <th scope="col">
+                <span 
+                  @click="sortByColumn('urlEnabled')"
+                  class="sort-by"
+                > Enabled 
+                </span>
+              </th>
+              <th scope="col">
+                Update
+              </th>
             </tr>
           </thead>
           <tbody v-for="url in urls" v-bind:key="url[0]" class="text-left">
@@ -37,17 +58,67 @@ export default {
     return {
       urls: '',
       dash: '',
-      isModalVisible: false,
+      sort: true,
+      endPointSortBy: 'ASC',
+      arrowup: 'keyboard_arrow_up',
+      arrowdown: 'keyboard_arrow_down'
     };
   },
 
   methods: {
     dashboard() {
-      return axios.get('http://localhost:5000/dashboard')
+      return axios.get('http://localhost:5000/dashboard', 
+      {
+        params: {
+          column: 'null',
+          sortBy: 'null'
+        },
+      })
         .then((res) => {
-          console.log(res.data[0])
+          console.log(res.data[0]);
           this.urls = res.data;
-          
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    sortByColumn(col) {
+      this.sort = !this.sort;
+
+      if (this.sort) {
+        this.endPointSortBy = 'ASC';
+        this.getRequestDash(col, this.endPointSortBy);
+      } 
+      else if (!this.sort) {
+        this.endPointSortBy = 'DESC';
+        this.getRequestDash(col, this.endPointSortBy);
+      }
+    },
+
+    sortByUrlType(col) {
+      this.sort = !this.sort;
+
+      if (this.sort) {
+        this.endPointSortBy = 'ASC';
+        this.getRequestDash(col, this.endPointSortBy);
+      } 
+      else if (!this.sort) {
+        this.endPointSortBy = 'DESC';
+        this.getRequestDash(col, this.endPointSortBy);
+      }
+    },
+
+    getRequestDash(endpoint, filter) {
+      return axios.get('http://localhost:5000/dashboard', {
+        params: {
+          column: endpoint,
+          sortBy: filter,
+        },
+      })
+        .then((res) => {
+          // console.log(res.data);
+          this.urls = res.data;
         })
         .catch((error) => {
           console.log(error);
@@ -58,7 +129,9 @@ export default {
   }, // end methods:
 
   computed: {
-
+    testComp() {
+      alert('Computed Ran');
+    },
   },
 
   created() {
@@ -71,6 +144,11 @@ export default {
 
 .btn-edit {
   padding: 0px 2px 0px 2px;
+}
+
+.sort-by {
+  text-decoration:underline;
+  cursor: pointer;
 }
 
 </style>
